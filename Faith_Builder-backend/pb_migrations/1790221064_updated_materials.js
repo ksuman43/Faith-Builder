2a7603c32889e04f401648b1235f3f590f3456ca
user.Grimm <aaron.grimm66@gmail.com>
2026-09-23 22:57:37 -0500

    chore: ignore personal study notes and local database files

---
+++ Faith_Builder-backend/pb_migrations/1790221064_updated_materials.js
@@ -0,0 +1,30 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_4282183725")
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text1874629670",
+    "max": 0,
+    "min": 0,
+    "name": "tags",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_4282183725")
+
+  // remove field
+  collection.fields.removeById("text1874629670")
+
+  return app.save(collection)
+})
